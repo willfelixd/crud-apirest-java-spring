867e7ecf031861843ce00d75063a2d042a702f79
willfelixd <willfelixd@gmail.com>
2024-08-15 20:31:50 -0300

    Primeiro commit

---
+++ src/main/java/com/unipe/crudapirestjavaspring/CrudApirestJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.unipe.crudapirestjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApirestJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApirestJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unipe/crudapirestjavaspring/CrudApirestJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipe.crudapirestjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApirestJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
