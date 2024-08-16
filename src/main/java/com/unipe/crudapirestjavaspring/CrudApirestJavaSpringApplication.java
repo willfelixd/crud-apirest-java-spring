1abd12eb510dfe7c9ac77c8f881afd9dfb800628
willfelixd <willfelixd@gmail.com>
2024-08-15 22:04:07 -0300

    Métodos get e set gerados

--- src/main/java/com/unipe/crudapirestjavaspring/CrudApirestJavaSpringApplication.java
+++ src/main/java/com/unipe/crudapirestjavaspring/CrudApirestJavaSpringApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrudApirestJavaSpringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CrudApirestJavaSpringApplication.class, args);
+
 	}
 
 }
